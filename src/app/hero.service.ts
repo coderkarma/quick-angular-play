a2cb1995f92f3cff914a42dda7d45cd6e641b52f
coderkarma <karmadrukpa11@gmail.com>
2020-03-12 15:26:12 -0700

    added save and add functionality plus in memomory-data service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
@@ -9,16 +11,62 @@ import { MessageService } from "./message.service";
   providedIn: "root"
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = "api/heroes";
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+  };
 
   getHeroes(): Observable<Hero[]> {
     // send the message after fetching the heroes
-    this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(__ => this.log("fetched heroes")),
+      catchError(this.handleError<Hero[]>("getHeroes", []))
+    );
   }
 
+  // GEt hero by id, will 404 if id not found
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // send the error to remote loggin infrastructure
+      console.error(error);
+
+      // better job of transforming errro for user consumption
+      this.log(`${operation} faiile: ${error.message}`);
+
+      // let the app keep running by retruning an empty result;
+      return of(result as T);
+    };
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>("updatedHero"))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/id=${newHero.id}`)),
+      catchError(this.handleError<Hero>("addHero"))
+    );
   }
 }
